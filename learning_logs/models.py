ef54449dc04473331ed1141288c8883734381dcc
Musbi <musbi700@gmail.com>
2025-08-17 01:44:00 +0100

    Basic database added

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """A topic the user in learning about."""
+    text = models.CharField(max_length=200) # create a text topic database for the user
+    author = models.CharField(max_length=200)
+    data_added = models.DateTimeField(auto_now_add=True) # set the currently time any time the user create a new topic
+
+    def __str__(self) :
+        """Return a string representating of the model."""
+        return self.text
