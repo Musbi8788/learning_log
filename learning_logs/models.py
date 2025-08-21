8c8d459181e81df1136e601b1e5a257784d820f4
Musbi <musbi700@gmail.com>
2025-08-21 14:52:50 +0100

    update models.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,8 +21,14 @@ class Entry(models.Model):
     
     
     class Mete:
-        verbuse_name_plunal = 'entries'
+        verbose_name_plunal = 'entries'
 
-    def __str__(self,):
+    def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
+        Max_Char = 50
+        if not self.text:
+            return ""
+        
+        return self.text if len(self.text) <= Max_Char else f"{self.text[:Max_Char]}..."
+
+        
