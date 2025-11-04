de62c9a4158a691451168a5217aa8a2014bc01a7
Musbi <musbi700@gmail.com>
2025-11-04 00:39:33 +0000

    Styled Base.html

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
-    path('', include("learning_logs.urls")),
+    path('', include("learning_logs.urls")),#home path
 ]
